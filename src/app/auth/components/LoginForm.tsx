b49589f57fdfa220bb7350adce156ef671112a3c
notHi3u <buihoanghieu21122003@gmail.com>
2024-08-27 14:19:19 +0700

    Login, sign up form

---
+++ src/app/auth/components/LoginForm.tsx
@@ -0,0 +1,83 @@
+'use client';
+import { LoginFormData } from "@/@types";
+import Link from "next/link";
+import { useState } from "react";
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState<LoginFormData>({ email: "", password: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log("Email:", formData.email);
+    console.log("Password:", formData.password);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-clear">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-md">
+        <h2 className="text-2xl font-bold text-center text-black">Login</h2>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-black"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-black"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
+          <div className="mt-4 text-center text-black">
+            <p>Don&apos;t have an account?</p>
+            <Link href="/auth/signup" className="text-blue-500 hover:underline">
+              Sign Up
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
